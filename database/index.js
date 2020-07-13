11c494d53ae188a2d49a1c8a94837572fa765c20
Aldrich Neil <aldrich.neil.hung@gmail.com>
2020-07-13 15:27:29 +0700

    change database url

--- database/index.js
+++ database/index.js
@@ -1,12 +1,20 @@
 const mysql = require('mysql');
 const util = require('util');
 
+// const db = mysql.createConnection({
+//     host: 'localhost',
+//     user: 'Aldrich',
+//     password: 'neil1804',
+//     database: 'rakata',
+//     port: 3306,
+// });
+
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'Aldrich',
-    password: 'neil1804',
-    database: 'rakata',
-    port: 3306,
+    host : 'db4free.net',
+    user : 'aldrich',
+    password : 'neil1804',
+    database : 'todoappneil',
+    port : 3306
 });
 
 const query = util.promisify(db.query).bind(db);
